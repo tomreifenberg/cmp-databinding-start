b9be09d6c1949cb01376c4f041270693a348ec10
unknown <reifenberg.57@gmail.com>
2021-03-08 08:57:55 -0600

    Binding to Custom Properties

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Server } from 'http';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  @Input() element: {type: string, name: string, content: string};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
