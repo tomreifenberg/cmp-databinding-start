1da51c83f25cf5321e4e238f4166cfda8f735718
unknown <reifenberg.57@gmail.com>
2021-03-14 19:36:25 -0500

    Seeing Lifecycle Hooks In Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 //import { Server } from 'http';
 
 @Component({
@@ -7,12 +20,57 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+   }
+
+   //good to use if you want to do something with previous value upon changing, like storing in DB, before it's dumped
+   ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit Called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked Called');
+
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit Called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked Called');
+
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestory Called!')
   }
 
 }
